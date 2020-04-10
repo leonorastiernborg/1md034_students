049ab7a0588c5766c5e07743cfbf1b1d41a34a95
Leonora Stiernborg <leonora.stiernborg@gmail.com>
2020-04-10 15:09:41 +0200

    task t4-4

---
+++ public/js/vue_script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+let burger1 = new  menuIteams('Halloumiburgare', '645 kcal',  'Halloumi, briochebröd och majonnäs',
+    'gluten', '60kr', '/img/burger1.jpg');
+let burger2 = new menuIteams('Halloumiburgaren2.0', '700 kcal',  'Umamidressing, halloumi, briochebröd',
+    'gluten, laktos', '70kr', '/img/burger2.jpg');
+let burger3 = new menuIteams('Halloumiburgaren3.0', '800 kcal',  'halloumi, lökringar, majonnäs',
+    'gluten, laktos', '80kr', '/img/burger3.jpg');
+let burger4 = new menuIteams('Vegburgare', '670 kcal',  'vegoburgare, majonnäs, sötpotatisbröd',
+    'gluten, laktos', '85kr', '/img/burger4.jpg');
+let burger5 = new menuIteams('Veganburgare', '720 kcal',  'veganburgare, vegan-majonnäs, sötpotatisbröd', 'gluten', '80kr', '/img/burger5.jpg');
+
+
+
+
+function menuIteams(n, k, i,a, p, pth){
+
+    this.namn = n;
+    this.cal = k;
+    this.ingredienser = i;
+    this.allergier = a;
+    this.price = p;
+    this.image =  pth;
+    this.iteam = function(){
+        return  this.namn + ' '  + this.cal ;
+    };
+
+}
